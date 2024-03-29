cfec97de1eca9ecb9e07332ebe8573e8036b9526
Muhammad Faisal <digital.1faisal@gmail.com>
2024-03-29 11:27:34 +0500

    updating site

--- PortfolioWebsite-HTML-CSS-00/script.js
+++ PortfolioWebsite-HTML-CSS-00/script.js
@@ -1,17 +1,17 @@
 const audio = new Audio("./audios/name-audio.mp3");
+const audioImage = document.querySelector(".name img");
 
 let playAudio = () => {
   audio.play();
 };
 
-document.querySelector(".name img").addEventListener("click", () => {
-  const image = document.querySelector(".name img");
-  image.style.opacity = "0.6";
-  image.style.cursor = "progress";
+audioImage.addEventListener("click", () => {
+  audioImage.style.opacity = "0.6";
+  audioImage.style.cursor = "progress";
   playAudio();
 
   setTimeout(() => {
-    image.style.opacity = "1";
-    image.style.cursor = "pointer";
+    audioImage.style.opacity = "1";
+    audioImage.style.cursor = "pointer";
   }, 1000);
 });
