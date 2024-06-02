f13980aba1304fde1ac3c4a18a487f10f41a800a
Muhammad Faisal <90336526+codebyfaisal@users.noreply.github.com>
2024-06-02 08:39:09 +0500

    Create script.js

---
+++ Javascript-Exercises/js-assignment-00/practice/script.js
@@ -0,0 +1,11 @@
+alert("Hello World!");
+alert(23);
+alert("Hello World! " + 23); // return string
+alert(22 + " Hello World!"); // return string
+alert(23 + 34); // return integer
+
+// 🚨 JavaScript Alert Function The alert() function in JavaScript displays a message in a modal dialog box.
+// It can be used to inform the user about specific actions or events within a web page.
+
+// 📝 Alert Usage The alert() function takes a string argument representing the message to be displayed.
+// It blocks further execution of JavaScript code until the user dismisses the dialog box.
