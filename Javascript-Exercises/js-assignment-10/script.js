99a809311e97c092daaefd40a5aacf5619448c16
Muhammad Faisal <90336526+codebyfaisal@users.noreply.github.com>
2024-07-21 10:16:19 +0500

    Update script.js

--- Javascript-Exercises/js-assignment-10/script.js
+++ Javascript-Exercises/js-assignment-10/script.js
@@ -1,1 +1,34 @@
-536022
+//* Convert Celsius to Fahrenheit *//
+// let celsius = +prompt("Please enter Temperature in Celsius");
+// let fahrenheit = (celsius * 9 / 5) + 32; // (0°C × 9/5) + 32 = 32°F
+// console.log(`${fahrenheit}°F`)
+
+//* Convert Fahrenheit to Celsius *//
+// let fahrenheit = +prompt("Please enter Temperature in Fahrenheit");
+// let celsius = (fahrenheit - 32) * 5 / 9; // (32°F − 32) × 5/9 = 0°C
+
+// console.log(`${celsius}°C`)
+
+//* Reverse an Array *//
+// let array = []; // initialize
+
+// for (let i = 0; i < 5; i++) {
+//     array[ i ] = +prompt(`Please enter number ${i+1}`); // userInput store in array
+// }
+// for (let i = array.length - 1; i >= 0; i--) {
+//     console.log(array[ i ])
+// }
+
+//* Average of an Array *//
+// let array = []; // initialize
+
+// for (let i = 0; i < 5; i++) {
+//     array[ i ] = +prompt(`Please enter number ${i + 1}`); // userInput store in array
+// }
+// let sum = 0;
+// for (let i = 0; i < array.length; i++) {
+//     sum += array[ i ]
+// }
+
+// let avg = sum / array.length;
+// console.log(avg)
